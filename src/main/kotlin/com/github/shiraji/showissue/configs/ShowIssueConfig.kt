2f4cf12baaeaec2ca23e74d919581a1c4a3a22c0
shiraji <isogai.shiraji@gmail.com>
2016-04-21 00:15:31 +0900

    Add has methods

--- src/main/kotlin/com/github/shiraji/showissue/configs/ShowIssueConfig.kt
+++ src/main/kotlin/com/github/shiraji/showissue/configs/ShowIssueConfig.kt
@@ -14,10 +14,16 @@ class ShowIssueConfig {
         fun getIssuePath(project: Project)
                 = PropertiesComponent.getInstance(project).getValue(ISSUE_PATH_KEY)
 
+        fun hasIssuePath(project: Project)
+                = PropertiesComponent.getInstance(project).isValueSet(ISSUE_PATH_KEY)
+
         fun setBranchRegEx(project: Project, regex: String)
                 = PropertiesComponent.getInstance(project).setValue(BRANCH_REGEX_KEY, regex)
 
         fun getBranchRegEx(project: Project)
                 = PropertiesComponent.getInstance(project).getValue(BRANCH_REGEX_KEY)
+
+        fun hasBranchRegEx(project: Project)
+                = PropertiesComponent.getInstance(project).isValueSet(BRANCH_REGEX_KEY)
     }
 }
