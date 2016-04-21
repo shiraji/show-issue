ce324687cbb42fa0f2a1c08b92e1d2d642621dd1
shiraji <isogai.shiraji@gmail.com>
2016-04-21 13:56:08 +0900

    Fix regular expression that accept all branch name

--- src/main/kotlin/com/github/shiraji/showissue/Extensions.kt
+++ src/main/kotlin/com/github/shiraji/showissue/Extensions.kt
@@ -4,6 +4,6 @@ import git4idea.GitLocalBranch
 
 // TODO make reg customizable.
 
-fun GitLocalBranch.getIssueIdFromBranchName() = "(\\d*)\\D.*".toRegex().find(this.name)!!.groups[1]!!.value.toInt()
+fun GitLocalBranch.getIssueIdFromBranchName() = "(\\d\\d*)\\D.*".toRegex().find(this.name)!!.groups[1]!!.value.toInt()
 
-fun GitLocalBranch.doesBranchHasIssueId() = "(\\d*)\\D.*".toRegex().matches(this.name)
+fun GitLocalBranch.doesBranchHasIssueId() = "(\\d\\d*)\\D.*".toRegex().matches(this.name)
